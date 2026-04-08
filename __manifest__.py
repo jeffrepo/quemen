6139d6b033e3a4327b75cfc2855e19bef45e1873
jeffrepo <jeffer.leo93@gmail.com>
2026-04-08 13:47:20 -0600

    Reporte xlsx

--- __manifest__.py
+++ __manifest__.py
@@ -50,6 +50,7 @@
         'views/reporte_entrega_valores.xml',
         'data/quemen_data.xml',
         'views/quemen_views.xml',
+        'views/inventory_xlsx_wizard_views.xml',
         # 'views/stock_quant_views.xml',
         'views/pos_session_view.xml',
         'views/res_partner_views.xml',
