7c84254757d7535d797f0f79bcdfb8aa8b95f8b7
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2026-07-14 08:38:12 -0600

    Uncomment pos_order_view.xml in manifest

--- __manifest__.py
+++ __manifest__.py
@@ -59,7 +59,7 @@
         'views/reporte_explosion_insumos.xml',
         'views/reporte_explosion_insumos_costo.xml',
         'views/mrp_bom_views.xml',
-        #'views/pos_order_view.xml',
+        'views/pos_order_view.xml',
         'views/quemen_pos_order_report_view.xml',
         # 'views/templates.xml',
         'views/reporte_codigo_barras.xml',
